8a6400b2b4e82d77cba0183b9473c35e8c9b7816
Barbará Silva <95880716+barbssx@users.noreply.github.com>
2024-04-06 18:41:06 -0300

    Ocultando dados do banco de dados

--- src/database/connection.js
+++ src/database/connection.js
@@ -1,11 +1,13 @@
 var knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host : 'database-2.clusseigsge6.us-east-2.rds.amazonaws.com',
+        host : '********.clusseigsge6.us-east-2.rds.amazonaws.com',
         user : 'barbss',
-        password : 'Ba661998',
-        database : 'dbAgenda',
+        password : '********',
+        database : 'db*****',
         port: 3306 
     }
 });
 module.exports = knex;
+
+//Dados ocultados por segurança 
